66b3662fce3077690a2d8ce38b91dff736898640
friedcheckin <friedcheckin@csserver.evansville.edu>
2012-11-15 21:52:29 -0600

    Remvoed duplicate migration and fixed errors in config files.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Friedcheckin::Application.config.session_store :cookie_store, key: '_friedcheckin_session'
+Friedcheckin::Application.config.session_store :cookie_store, :key => '_friedcheckin_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
