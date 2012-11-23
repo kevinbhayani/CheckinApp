5f73a4731d0d5fbc26934ab63040a7100e5f6bf9
McIntyre Watts <mw229@evansville.edu>
2012-11-23 15:34:58 -0600

    Changed routesfile to load events after login info. McIntyre & Melanie

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Friedcheckin::Application.routes.draw do
-  resources :events
   devise_for :users do get '/users/sign_out' => 'devise/sessions#destroy' end
-
+  resources :events
   resources :students 
   resources :volunteers
   match ':controller(/:action(/:name/:pin))'
   match ':controller(/:action(/:getStudents))'
   match ':controller(/:action(/:newStudent/:student))'
-  root :to => redirect('/students')
+  root :to => redirect('/friedcheckin/students')
 end
