e0f807dabd07dbd0da921b38b19145d14ef85efb
Kevin <kb279@evansville.edu>
2012-12-03 12:38:10 -0800

    Modified checkin view and controller for testing

--- app/controllers/checkin_controller.rb
+++ app/controllers/checkin_controller.rb
@@ -7,28 +7,23 @@ respond_to :html, :json
   
   def index
       @admin_template = false
-      @studentsEvents = StudentsEvent.all 
+      @studentsEvents = StudentsEvent.all
       @students = Student.all
       @events = Event.all
       @selected_id=0
-      respond_with(@studentsEvents)    
+      respond_with @studentsEvents     
   end
 
   def create
-    # browser = Browser.new(:ua => request.user_agent)
-    # if browser.name == "other"
-      checkin_info = JSON.parse(request.body.read)
-      # @checkin = StudentsEvent.create!(checkin_info)
-      # render :json => @checkin
-      # logger.debug "tttttttttttttttttttttt"
-    # # else
-      # @admin_template = false
       @students = Student.all
       @events = Event.all
-      @studentsEvents = StudentsEvent.all 
+      @studentsEvents = StudentsEvent.all
+
+      checkin_info = "1"#JSON.parse(request.body.read)
+ 
       if checkin_info['checkin_status'].eql? true
         @checkin = StudentsEvent.create!(checkin_info)
-        @checkin["student_name"] = Student.find(checkin_info['student_id']).name
+        #@checkin["student_name"] = Student.find(checkin_info['student_id']).name
         render :json => @checkin
       
       elsif checkin_info['checkin_status'].eql? false
@@ -36,27 +31,27 @@ respond_to :html, :json
         @checkout.checkin_status = false
         @checkout.save
         render :json => @checkout 
-      # else        
-#        
-        # if not params[:checkout].nil?
-          # @change = StudentsEvent.find params[:checkout]
-          # @change.checkin_status = false
-          # @change.save
-#         
-        # else
-          # redirect_to checkin_index_path and show_error_message and return if params[:student_id].eql? "0" or params[:event_id].eql? "0"
-          # @student = Student.find params[:student_id]
-          # @event = Event.find params[:event_id]
-#   
-          # begin
-            # StudentsEvent.create!({"student_id" => params[:student_id], "event_id" => params[:event_id]})
-          # rescue
-            # flash[:notice] = "Error: Student '#{@student.name}' is already Checked In at Event '#{@event.name}'."
-          # else                              
-            # flash[:notice] = "Student '#{@student.name}' Checked In at Event '#{@event.name}'."
-          # end
-        # end       
-        # redirect_to checkin_index_path      
+      else        
+       
+        if not params[:checkout].nil?
+          @change = StudentsEvent.find params[:checkout]
+          @change.checkin_status = false
+          @change.save
+        
+        else
+          redirect_to checkin_index_path and show_error_message and return if params[:student_id].eql? "0" or params[:event_id].eql? "0"
+          @student = Student.find params[:student_id]
+          @event = Event.find params[:event_id]
+  
+          begin
+            StudentsEvent.create!({"student_id" => params[:student_id], "event_id" => params[:event_id]})
+          rescue
+            flash[:notice] = "Error: Student '#{@student.name}' is already Checked In at Event '#{@event.name}'."
+          else                              
+            flash[:notice] = "Student '#{@student.name}' Checked In at Event '#{@event.name}'."
+          end
+        end       
+        redirect_to checkin_index_path              
       end
   end
  
